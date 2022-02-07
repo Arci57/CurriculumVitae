06c50c77ed663206dea4dd13907ee52f50f949c2
Arci <arci@example.org>
2022-02-07 13:59:01 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function nome(){
+	var nome = document.getElementById("nome").value
+
+	if (value == null || value.isempty()) {
+		return false
+	}
+	else {
+		return true;
+	}
+}
+
+function cognome(){
+	var cognome = document.getElementById("cognome").value
+
+	if (value == null || value.isempty()) {
+		return false
+	}
+	else {
+		return true;
+	}
+}
